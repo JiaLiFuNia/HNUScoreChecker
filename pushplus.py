f58ffe21d3656a892246c5b44fa48eee0149ed94
许 博涵 <woshixubohan04@163.com>
2024-08-04 15:35:57 +0800

    Update pushplus.py

--- pushplus.py
+++ pushplus.py
@@ -5,7 +5,7 @@ import os
 
 def send_message(title, content):
 # 不要用我的token(*´I`*)
-    token = "0ee3d58a9bfe4dc2a7127720ba0f9edc"
+    token = os.environ.get("PUSH_PLUS_TOKEN")
     url = "http://www.pushplus.plus/send"
     data = {"token": token, "title": title, "content": content, "template": 'markdown'}
     body = json.dumps(data).encode(encoding="utf-8")
